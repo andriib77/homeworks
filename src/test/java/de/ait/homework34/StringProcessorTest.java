3420421c422f5d37fce2317eef9dae51f0ac9da0
Andrii Bychenko <bychenkoandrii@gmail.com>
2023-10-08 17:46:48 +0200

    34: Unit-test for StringProcessor

--- src/test/java/de/ait/homework34/StringProcessorTest.java
+++ src/test/java/de/ait/homework34/StringProcessorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringProcessorTest {
     private StringProcessor stringProcessor;
@@ -14,13 +16,56 @@ public class StringProcessorTest {
     }
 
     @Test
-    public void testConcatenate(){
-        assertEquals("hallo", stringProcessor.concatenate("hal", "lo"));
-    }
-    @Test
-    public void testConcatenateNegative(){
+    public void testConcatenatePositive() {
         assertEquals("hallo Deutschland", stringProcessor.concatenate("hallo ", "Deutschland"));
     }
 
+    public void testConcatenateNegative() {
+        assertEquals("", stringProcessor.concatenate("", ""));
+    }
 
+    @Test
+    public void testLength() {
+        assertEquals(17, stringProcessor.getLength("hallo Deutschland"));
+    }
+
+    @Test
+    public void testLengthNoData() {
+        assertEquals(0, stringProcessor.getLength(""));
+    }
+
+    @Test
+    public void testLengthSymbols() {
+        assertEquals(4, stringProcessor.getLength(" @# "));
+    }
+
+    @Test
+    public void testReverse() {
+        assertEquals("iirdnA", stringProcessor.reverse("Andrii"));
+    }
+
+    @Test
+    public void testReverseLeer() {
+        assertEquals("", stringProcessor.reverse(""));
+    }
+
+    @Test
+    public void testReverseSymbols() {
+        assertEquals(" #@ ", stringProcessor.reverse(" @# "));
+    }
+
+    @Test
+    public void testPalindromeTrue() {
+        assertTrue(stringProcessor.isPalindrome("lagerregal"));
+    }
+
+    @Test
+    public void testPalindromeLeerTrue() {
+        assertTrue(stringProcessor.isPalindrome(""));
+    }
+    @Test
+    public void testPalindromeFalse() {
+        assertFalse(stringProcessor.isPalindrome("hello"));
+    }
 }
+
